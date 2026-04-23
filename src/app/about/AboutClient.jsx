ea25c677fe4df1575bbdf4d2946be1da4cdab8c2
Kuldeep Kumar Kandu <kulkandu@publicisgroupe.net>
2026-04-23 13:48:16 +0530

    made changes in about section for ssg

--- src/app/about/AboutClient.jsx
+++ src/app/about/AboutClient.jsx
@@ -5,10 +5,30 @@ import AboutLanding from "../../components/aboutSection/aboutLanding";
 import Expertise from "../../components/aboutSection/Expertise";
 import TechnologyArsenal from "../../components/aboutSection/TechnologyArsenal";
 import MessageCard from "../../components/aboutSection/MessageCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getSkillsAndCategories } from "../../services/skillsCategory";
 
 export default function AboutClient({ initialTech = [] }) {
   const [tech, setTech] = useState(initialTech);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const loadLatestSkills = async () => {
+      try {
+        const data = await getSkillsAndCategories();
+        if (isMounted && Array.isArray(data?.skills)) {
+          setTech(data.skills);
+        }
+      } catch {}
+    };
+
+    loadLatestSkills();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   const handleDeleteSkillCategory = (id) => {
     setTech((prev) => prev.filter((group) => Number(group.id) !== Number(id)));
